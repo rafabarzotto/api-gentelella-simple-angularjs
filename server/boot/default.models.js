7095ca2df00fe360f41b28ccfac885de10115c0f
Rafael Barzotto <rafabarzotto@hotmail.com>
2017-01-19 16:48:15 -0200

    fixing for deploy

--- server/boot/default.models.js
+++ server/boot/default.models.js
@@ -1,38 +1,33 @@
-// module.exports = function(app) {
-//   var User = app.models.user;
-//   var Role = app.models.Role;
-//   var RoleMapping = app.models.RoleMapping;
+module.exports = function(app) {
+  var User = app.models.user;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
 
-//   User.create([
-//     {name: "Rafael Barzotto", username: 'rafael', email: 'rafabarzotto@hotmail.com', password: 'qwe123', routes: ['/preco', '/teste', '/dash', '/naosei']},
-//     {name: "Leonardo dos Santos", username: 'leo', email: 'leodoima@hotmail.com', password: 'qwe123', routes: ['/users', '/teste', '/dash']},
-//     {name: "Marcelo Camillo", username: 'marcelo', email: 'marcelo@hotmail.com', password: 'qwe123', routes: ['/users', '/teste', '/dash']}
-//   ], function(err, users) {
-//     if (err) throw err;
+  User.create([
+    {name: "Rafael Barzotto", username: 'rafael', email: 'rafabarzotto@hotmail.com', password: 'qwe123', routes: ['/preco', '/teste', '/dash', '/naosei']},
+    {name: "Leonardo dos Santos", username: 'leo', email: 'leodoima@hotmail.com', password: 'qwe123', routes: ['/users', '/teste', '/dash']},
+    {name: "Marcelo Camillo", username: 'marcelo', email: 'marcelo@hotmail.com', password: 'qwe123', routes: ['/users', '/teste', '/dash']}
+  ], function(err, users) {
+    if (err) throw err;
 
-//     console.log('Created users:', users);
+    console.log('Created users:', users);
 
-//     //create the admin role
-//     Role.create({
-//       name: 'admin'
-//     }, function(err, role) {
-//       if (err) throw err;
+    //create the admin role
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      if (err) throw err;
 
-//       console.log('Created role:', role);
+      console.log('Created role:', role);
 
-//       role.principals.create({
-//         principalType: RoleMapping.USER,
-//         principalId: users[0].id
-//       }, function(err, principal) {
-//         if (err) throw err;
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[0].id
+      }, function(err, principal) {
+        if (err) throw err;
 
-//         console.log('Created principal:', principal);
-//       });
-//     });
-//   });
-// };
-
-// {
-//   "username":"rafael",
-//   "password":"qwe123"
-// }
+        console.log('Created principal:', principal);
+      });
+    });
+  });
+};
